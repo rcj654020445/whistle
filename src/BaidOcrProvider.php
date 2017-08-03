9ade6213af645d921b7f886e0a94ac2fb4fa9804
MrTom <540054895@qq.com>
2017-08-03 17:52:26 +0800

    init

---
+++ src/BaidOcrProvider.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: RCJ
+ * Date: 2017/8/2
+ * Time: 14:50
+ */
+
+namespace RCJ\Ocr;
+
+use Illuminate\Support\ServiceProvider;
+use RCJ\Ocr\AipOcr;
+
+class BaidOcrProvider extends ServiceProvider
+{
+    /**
+     * 服务提供者加是否延迟加载.
+     *
+     * @var bool
+     */
+    protected $defer = true;
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //配置
+        $this->publishes([
+            __DIR__.'\ocr.php' => config_path('ocr.php'),
+        ]);
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('AipOcr',function ($app) {
+            return new AipOcr(config('ocr.APP_ID'),config('ocr.API_KEY'),config('ocr.SECRET_KEY'));
+        });
+    }
+
+    public function provides()
+    {
+        return array('AipOcr');
+    }
+
+    public function __call($name, $arguments)
+    {
+        echo '3';
+        exit();
+        // TODO: Implement __call() method.
+    }
+}
